29ff0cfacd7ef1de2eb4316522ad673a2b438a02
gaboluque <gluquemiranda@gmail.com>
2021-02-18 16:48:20 +0000

    Deploying to gh-pages from  @ 623ab5eb9daad97c55e5ce366a50feab501464ef 🚀

--- _next/static/pHBy287tuSb2C_MO0NTB1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-963e18d6e2f43e5b6c90.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-066d51c78a9a057661af.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
